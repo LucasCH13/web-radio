245ca525a71e136d8d1702466f6224603d48cf35
Lucas <lucas.charrier13@gmail.com>
2020-11-10 16:30:04 +0100

    test d'envoie de requête

--- maRadio/src/Controller/AdminController.php
+++ maRadio/src/Controller/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+
 
 
 class AdminController extends AbstractController {
@@ -17,9 +19,24 @@ class AdminController extends AbstractController {
     }
     /**
      * @Route("/administration", name="button_test")
+     * 
      */
-    public function buttonCall() {
-       
+    public function buttonCall(): Response {
+       //j99MYuH7ghxGwq5p
+    //http://api.shoutcast.com/legacy/genresearch?k=j99MYuH7ghxGwq5p&genre=classic&limit=5,4
 
+    $httpClient = HttpClient::create();
+    $response = $httpClient->request('GET', 'http://api.shoutcast.com/legacy/genresearch?k=j99MYuH7ghxGwq5p&genre=Rap&limit=2,1', [
+        'headers' => [
+            'Accept' => 'application/xhtml+xml',
+        ],
+     ]);  
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+
+       return new Response(
+        '<html><body>'.$content.'</body></html>'
+    );
     }
 }
