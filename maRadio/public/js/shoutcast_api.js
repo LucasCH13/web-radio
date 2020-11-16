ecc524f18f34391892ae58821e83cc665b3e5adf
Lucas <lucas.charrier13@gmail.com>
2020-11-16 17:00:59 +0100

    Afficher une radio par genre

--- maRadio/public/js/shoutcast_api.js
+++ maRadio/public/js/shoutcast_api.js
@@ -1,6 +1,7 @@
 let selectedValue;
 let api_key = "j99MYuH7ghxGwq5p";
 let url_genre;
+let tuneInUrl = " http://yp.shoutcast.com"
 ////http://api.shoutcast.com/legacy/genresearch?k=j99MYuH7ghxGwq5p&genre=Classic&limit=2,1
 
 $(document).ready(function(){
@@ -13,14 +14,31 @@ $(document).ready(function(){
   });
 
 function getRadioByGenre() {  
-/*
+
     $.ajax({
         type: "GET",
+        crossDomain: true,
         url: url_genre,
-        dataType: "xml",
-        success: function (xml_response) {
-            
-        }
+        //problème CORS
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        dataType: "xml",    
+        success: function (xmlResponse) {
+            $xmlParse = $.parseXML(xmlResponse);
+            $xml = $($xmlParse);
+            $tuneInVal = $(xml).find("tunein").text();
+            $stationId = $(xml).find("station").each(function () { 
+                    var id = $(this).attr('id');
+                    return id;
+             });
+             var audioTag = document.createElement("audio");
+                audioTag.src = $tuneInUrl + $tuneInVal + "?id=" + $stationId;
+                audioTag.type = 'audio/mpeg';
+                document.getElementById('api-content-result').appendChild(audioTag);
+        },
+        error: function (xmlResponse) { 
+            console.log("Une erreur est survenue");
+
+         }
     });
-*/
+
 }
