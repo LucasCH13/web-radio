5941390ca13b9fcc6b8b0776f81e495e6d2d0f01
Lucas <lucas.charrier13@gmail.com>
2020-11-26 16:47:02 +0100

    Affichage brut de la réponse

--- maRadio/src/Controller/AdminController.php
+++ maRadio/src/Controller/AdminController.php
@@ -2,44 +2,59 @@
 // src/Controller/AdminController.php
 namespace App\Controller;
 
-use Doctrine\DBAL\Abstraction\Result;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminController extends AbstractController {
     
     /**
      * @Route("/administration", name="admin") 
+     * Affiche la page
      */
     public function index_admin() {
         return $this->render('admin.html.twig', [
         ]);
         
     }
-    
-    public function shoutcast_api_call(): Response {
+    /**
+     * @Route("/")
+     * Affiche une station random 
+     * 
+     */
+    public function shoutcastAPI_random(): Response {
        //j99MYuH7ghxGwq5p
     //http://api.shoutcast.com/legacy/genresearch?k=j99MYuH7ghxGwq5p&genre=classic&limit=5,4
-        $url_station_random = '';
+       
+        //$response ='';
         $urlStation = '';
-        $httpClient = HttpClient::create();
-       $urlStation = isset($_POST[$url_station_random]);
-               $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&genre=Rock&limit=1' );  
-                
-                    $content = $response->getContent();
-                    //$content = $response->toArray();
-                    return new Response($content);
-                    
-                   
-                return $this->render('index.html.twig', [
-                    'api_content' => $content,
-                ]);
+        $url_station_random = isset($_POST["url_station_random"]);
+        $selectedGenre = isset($_POST["selectedGenre"]);
+        $tunein_url = isset($_POST["tunein_url"]);
+                $httpClient = HttpClient::create();
             
-    }
+                    $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&genre=&limit=1', [
+                        'query' => [
+                            'genre' => $selectedGenre,
+                        ],
+                       
+                    ]);  
+                        
+                            //$content = $response->getContent();
+                            
+                            $content = $response->toArray();//
+                            
+                  return new Response(
+                            $this->renderView('index.html.twig',  [
+                                'data' => $content,
+                                'tuneIn' => $tunein_url,
+                            
+                            ]));
+            
 
+    }
     
 }
