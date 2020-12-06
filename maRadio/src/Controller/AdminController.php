61eddb24e9f5202fa282e20e4793d3407fe37606
Lucas <lucas.charrier13@gmail.com>
2020-12-06 20:03:34 +0100

    Ajout du nom de la station radio

--- maRadio/src/Controller/AdminController.php
+++ maRadio/src/Controller/AdminController.php
@@ -23,24 +23,21 @@ class AdminController extends AbstractController {
     }
     /**
      * @Route("/")
-     * Affiche une station random 
+     * Affiche une radio aléatoire 
      * 
      */
     public function shoutcastAPI_random(): Response {
-       //j99MYuH7ghxGwq5p
-    //http://api.shoutcast.com/legacy/genresearch?k=j99MYuH7ghxGwq5p&genre=classic&limit=5,4
         $selectedGenre = isset($_POST["selectedGenre"]);
-       
+        
                 $httpClient = HttpClient::create();
-            
+                    //requête vers l'api
                     $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&genre=&limit=1', [
                         'query' => [
                             'genre' => $selectedGenre,
                         ],
                        
                     ]);
-                   
-                            //$content = $response->getContent();
+
                             $content = $response->toArray();
                             //on récupère dans $id et $base les valeurs pour construire l'url  :  http://yp.shoutcast.com/<base>?id=<id>
                             foreach($content as $arr)
@@ -50,14 +47,14 @@ class AdminController extends AbstractController {
                                     $id = $val["station"]["id"];
                                     $base = $val["tunein"]["base"];
                                     $logo = $val["station"]["logo"];
+                                    $name = $val["station"]["name"];
                                 } 
                             }
-                             
+                             //On récupère le contenue du fichier .pls de l'API pour obtenir le lien audio de la radio
                              $lines =  file_get_contents("http://yp.shoutcast.com/".$base."?id=".$id. "");
                              $lien_audio = preg_split("/[\s,]+/", $lines);
                              $lien_audio = str_replace("File1=", "", $lien_audio);
-
-                        
+                             
                   return new Response(
                             $this->renderView('index.html.twig',  [
                                 'content' => $content,
@@ -65,6 +62,7 @@ class AdminController extends AbstractController {
                                 'base' => $base,
                                 'url_audio' => $lien_audio[2],
                                 'logo' => $logo,
+                                'name' => $name,
 
                             ]));
                           
