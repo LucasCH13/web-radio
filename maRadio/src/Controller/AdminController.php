333da6a48987baeeeae929ff5bea38ea94f47f98
Lucas <lucas.charrier13@gmail.com>
2020-12-06 19:22:45 +0100

    Affiche une radio aléatoire sur la page d'accueil

--- maRadio/src/Controller/AdminController.php
+++ maRadio/src/Controller/AdminController.php
@@ -49,49 +49,23 @@ class AdminController extends AbstractController {
                                 {
                                     $id = $val["station"]["id"];
                                     $base = $val["tunein"]["base"];
+                                    $logo = $val["station"]["logo"];
                                 } 
                             }
-                            /*
-                            $line = 0;
-                                    $fh = fopen($myFile, 'r');
-
-                                    while (($buffer = fgets($fh)) !== FALSE) {
-                                    if ($line == 1) {
-                                        // $buffer is the second line.
-                                        break;
-                                    }   
-                                    $line++;
-                                    }
-                            */
-                           
-                           // $fichier = fopen("http://yp.shoutcast.com/".$base."?id=".$id. "", "r");
-                           /*
-                            if ($fichier) { 
-                                while (!feof($fichier)) { 
-                                    $lines[] = fgets($fichier, 4096); 
-                                } 
-                                fclose($fichier); 
-                                preg_split("/(=)/", $lines);
-                             } */
-                             //$lines = file("http://yp.shoutcast.com/".$base."?id=".$id. "", FILE_IGNORE_NEW_LINES);
                              
-                             $lines = explode("=", file_get_contents("http://yp.shoutcast.com/".$base."?id=".$id. ""));
-                             
-                             
+                             $lines =  file_get_contents("http://yp.shoutcast.com/".$base."?id=".$id. "");
+                             $lien_audio = preg_split("/[\s,]+/", $lines);
+                             $lien_audio = str_replace("File1=", "", $lien_audio);
 
-                             /*
-                             $str = 'Monsters are SUPER scary, bro!';
-                                $del = array('a', 'b', 'c');
-
-                                // In one fell swoop...
-                                $arr = explode( $del[0], str_replace($del, $del[0], $str) ); 
-                             */
+                        
                   return new Response(
                             $this->renderView('index.html.twig',  [
                                 'content' => $content,
                                 'data' => $id,
                                 'base' => $base,
-                                'url_audio' => $lines[2],
+                                'url_audio' => $lien_audio[2],
+                                'logo' => $logo,
+
                             ]));
                           
                          
