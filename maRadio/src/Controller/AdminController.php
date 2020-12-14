943cd0d88563103637813a75130516440bf93ced
Lucas <lucas.charrier13@gmail.com>
2020-12-14 17:56:19 +0100

    Affichage station par genre

--- maRadio/src/Controller/AdminController.php
+++ maRadio/src/Controller/AdminController.php
@@ -2,15 +2,12 @@
 // src/Controller/AdminController.php
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
-
-class AdminController extends AbstractController {
+class AdminController extends AbstractController  {
     
     /**
      * @Route("/administration", name="admin") 
@@ -27,17 +24,14 @@ class AdminController extends AbstractController {
      * 
      */
     public function shoutcastAPI_random(): Response {
-        $selectedGenre = isset($_POST["selectedGenre"]);
-        
                 $httpClient = HttpClient::create();
-                    //requête vers l'api
-                    $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&genre=&limit=1', [
-                        'query' => [
-                            'genre' => $selectedGenre,
-                        ],
-                       
-                    ]);
 
+                    //requête vers l'api
+                   
+                        $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&limit=1', [
+                        
+                        ]); 
+                                               
                             $content = $response->toArray();
                             //on récupère dans $id et $base les valeurs pour construire l'url  :  http://yp.shoutcast.com/<base>?id=<id>
                             foreach($content as $arr)
@@ -50,25 +44,57 @@ class AdminController extends AbstractController {
                                     $name = $val["station"]["name"];
                                 } 
                             }
-                             //On récupère le contenue du fichier .pls de l'API pour obtenir le lien audio de la radio
+                             //On récupère le contenu du fichier .pls de l'API pour obtenir le lien audio de la radio
                              $lines =  file_get_contents("http://yp.shoutcast.com/".$base."?id=".$id. "");
                              $lien_audio = preg_split("/[\s,]+/", $lines);
-                             $lien_audio = str_replace("File1=", "", $lien_audio);
+                             $lien_audioValid = str_replace("File1=", "", $lien_audio);
                              
                   return new Response(
                             $this->renderView('index.html.twig',  [
-                                'content' => $content,
-                                'data' => $id,
-                                'base' => $base,
-                                'url_audio' => $lien_audio[2],
+                                'url_audio' => $lien_audioValid[2],
                                 'logo' => $logo,
                                 'name' => $name,
-
                             ]));
-                          
-                         
-             
-                  
+
     }
     
+    /**
+     * @Route("/genre", name="genre")
+     * Affiche sur la page /genre une station basée sur le genre sélectionné
+     */
+    public function shoutcastAPI_byGenre(): Response {
+        $genre = $_POST['genreName'];
+        $httpClient = HttpClient::create();
+
+        //requête avec paramètre query prenant en compte le genre choisis
+          $response = $httpClient->request('GET',  'http://api.shoutcast.com/station/randomstations?k=j99MYuH7ghxGwq5p&f=json&mt=audio/mpeg&br=128&genre=&limit=1', [
+                        'query' => [
+                            'genre' => $genre, 
+                            ]
+                        ]); 
+                           
+                        $content = $response->toArray();
+                        foreach($content as $arr)
+                        {
+                            foreach($arr["data"] as $val) 
+                            {
+                                $id = $val["station"]["id"];
+                                $base = $val["tunein"]["base"];
+                                $logo = $val["station"]["logo"];
+                                $name = $val["station"]["name"];
+                            } 
+                        }
+                             $lines =  file_get_contents("http://yp.shoutcast.com/".$base."?id=".$id. "");
+                             $lien_audio = preg_split("/[\s,]+/", $lines);
+                             $lien_audioValid = str_replace("File1=", "", $lien_audio);
+
+          return new Response(
+              $this->renderView('genre.html.twig', [
+                  'url_audioGenre' => $lien_audioValid[2],
+                  'logoGenre' => $logo,
+                  'nameGenre' => $name,
+              ])
+          );
+        
+    }
 }
